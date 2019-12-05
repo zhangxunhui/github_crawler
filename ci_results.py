558e806b3eeacbc2f6a90df8f67829dbb5727e82
nigel007 <609441862@qq.com>
2019-12-05 11:00:24 +0100

    add shell

--- ci_results.py
+++ ci_results.py
@@ -70,7 +70,7 @@ def crawl(attr):
     # print "handling project_id: %d, github_id: %d" % (project_id, github_id)
     url = "https://github.com/" + projectDict[project_id]["ownername"] + "/" + projectDict[project_id][
         "reponame"] + "/pull/" + str(github_id)
-    # print url
+    print url
 
     # user login
     cookie = "_ga=GA1.2.1107768544.1561208065; _octo=GH1.1.1372982601.1561208065; _device_id=4f0d01814a2edb7eb0633d41e0f851ed; ajs_user_id=null; ajs_group_id=null; ajs_anonymous_id=%2235373192-66d9-449b-b750-5787e31158e7%22; user_session=0ci29p2I-jZE4CQZNJ0Nia8GiTlqVa-DkN5yy6J2pa-1_jEa; __Host-user_session_same_site=0ci29p2I-jZE4CQZNJ0Nia8GiTlqVa-DkN5yy6J2pa-1_jEa; logged_in=yes; dotcom_user=nigel007; has_recent_activity=1; _gat=1; tz=Europe%2FAmsterdam; mp_167d2b841f875d338e9aa341312533dc_mixpanel=%7B%22distinct_id%22%3A%20%2216b7f408dd4521-03d3f2e8b7e682-37677e05-fa000-16b7f408dd5402%22%2C%22%24initial_referrer%22%3A%20%22https%3A%2F%2Fwww.iconfont.cn%2F%22%2C%22%24initial_referring_domain%22%3A%20%22www.iconfont.cn%22%2C%22__alias%22%3A%20%22anonymous%408057411815%22%2C%22%24search_engine%22%3A%20%22google%22%7D; _gh_sess=TE9SaUh6a01EdnphN3lVSi9qYWJFZm5RLy9NWW0ydktKa0JHbU5MU3NZUEN4RjVKOFNUZStjTEF2NVp0ZzVSUnQrL0xiSGMyMVVac1NlWGsySlNTbWgzcXBkVHVUcUNjc0d5ZkFwdkQ2YXhaQlNtbVhUbkQ5eTV6VmRqaUcrZ3Z0VFpWNFBab3pNTUFDanY4cUtLYzdoYjlCd1JZOXZ1dUJTOWxrMWpqQnZvPS0tTFJqc1dTWVpIU3FRdTRjbjA3d1Y0Zz09--92132ff6f4b8a81372d01559dc7bc3119bd4cfe5"
@@ -131,18 +131,10 @@ def newLoop():
         return
 
     # put all the tasks into the pool
-    pool.map(crawl, tasks)
+    pool.imap_unordered(crawl, tasks)
     pool.close()
     pool.join()
 
 
 if __name__ == "__main__":
-    while(True):
-        try:
-            newLoop()
-        except Exception as e:
-            print str(e)
-            time.sleep(10)
-            pass
-        else:
-            break
+    newLoop()
